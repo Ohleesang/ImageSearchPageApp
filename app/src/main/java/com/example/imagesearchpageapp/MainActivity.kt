5f787ca1cd9d2367d1d76353257f2336246afabf
Ohleesang <jammmmed@gmail.com>
2024-01-23 17:21:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/imagesearchpageapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.imagesearchpageapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imagesearchpageapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
