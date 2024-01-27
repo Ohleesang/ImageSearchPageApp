e37d88c6b28f2f8a7ff42ffb7f0ce190c6c2a8ef
Ohleesang <jammmmed@gmail.com>
2024-01-27 23:29:47 +0900

    Feat : BottomNavigationView 으로 변경

---
+++ app/src/main/java/com/example/imagesearchpageapp/ui/dashboard/MyPageFragment.kt
@@ -0,0 +1,41 @@
+package com.example.imagesearchpageapp.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.imagesearchpageapp.databinding.FragmentMyPageBinding
+
+class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMyPageBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(MyPageViewModel::class.java)
+
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
