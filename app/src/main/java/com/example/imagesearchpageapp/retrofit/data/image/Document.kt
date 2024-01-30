859111dd015867d1cd7da062a44b70761918a178
Ohleesang <jammmmed@gmail.com>
2024-01-30 10:46:29 +0900

    Feat : Video 검색 추가 - layout 문자 부분 수정 필요 - 데이터 관련해서 정리해야할 필요가 있음

--- app/src/main/java/com/example/imagesearchpageapp/retrofit/data/image/Document.kt
+++ app/src/main/java/com/example/imagesearchpageapp/retrofit/data/image/Document.kt
@@ -3,16 +3,11 @@ package com.example.imagesearchpageapp.retrofit.data.image
 import com.google.gson.annotations.SerializedName
 
 data class Document(
-//    val collection: String,
     @SerializedName("datetime")
     var dateTime: String,//dateTime
 
     @SerializedName("display_sitename")
     var siteName: String,//SiteName
-//    val doc_url: String,
-//    val height: Int,
-//    val image_url: String,
     @SerializedName("thumbnail_url")
     val thumbNailUrl: String,//Thumnail_Url
-//    val width: Int
 )
